03528e2990b3c011909e2eea97361ae72f1f489a
Bilal Sattar <bilalsattar24@gmail.com>
2022-09-01 23:03:30 -0700

    run prettier on all

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
+  const testValue = 'testValue';
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>Welcome to BilalSattar.com</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
