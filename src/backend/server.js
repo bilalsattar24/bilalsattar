2182337edda06e4eb4ad5fc4889e460c5805b6ff
Bilal Sattar <bilalsattar24@gmail.com>
2022-09-03 20:43:59 -0700

    update static folder

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const localPort = 4160;
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/ping', (req, res) => {
   return res.send('pong');
@@ -11,7 +11,6 @@ app.get('/ping', (req, res) => {
 
 app.get('/', (req, res) => {
   return res.send('homepage works');
-  // res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.get('/react', (req, res) => {
